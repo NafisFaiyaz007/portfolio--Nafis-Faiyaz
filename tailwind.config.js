6c81f70d816aa7cfc9f16883a5f68593970624d4
Nafis Faiyaz <56973454+NafisFaiyaz007@users.noreply.github.com>
2023-02-23 18:38:39 -0400

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+@import 'tailwindcss/base';
+@import 'tailwindcss/components';
+@import 'tailwindcss/utilities';
+
+module.exports = {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
