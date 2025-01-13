7be7092d400d03307a04d9c5540df48cc022edab
NafisFaiyaz007 <56973454+NafisFaiyaz007@users.noreply.github.com>
2025-01-13 15:06:08 +0600

    2025

---
+++ cnf/src/components/data/educationData.js
@@ -0,0 +1,17 @@
+const educationData = [
+  {
+    degree: "Bachelor of Science in Computer Science and Engineering",
+    institution: "North South University, Bangladesh",
+    duration: "2019 - 2024",
+    Major: "Software Engineering",
+    thesis: "Thesis: Blockchain-based Electronic Health Record Platform",
+  },
+  {
+    degree: "O&A Levels",
+    institution: "Mastermind English Medium School, Bangladesh",
+    duration: "2017 - 2019",
+    Major: "Focused on core science subjects.",
+  },
+];
+
+export default educationData;
